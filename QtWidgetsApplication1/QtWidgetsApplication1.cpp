f2450ac442637405f872b4f803af7c0a1ff15cf6
ThomArmax <esvcorp@gmail.com>
2020-10-28 10:34:44 +0100

    Initial commit

---
+++ QtWidgetsApplication1/QtWidgetsApplication1.cpp
@@ -0,0 +1,8 @@
+#include "QtWidgetsApplication1.h"
+#include "stdafx.h"
+
+QtWidgetsApplication1::QtWidgetsApplication1(QWidget *parent)
+    : QMainWindow(parent)
+{
+    ui.setupUi(this);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# dummy-qtvstool260-test-app
+
+Dummy test app for https://bugreports.qt.io/browse/QTVSADDINBUG-836
